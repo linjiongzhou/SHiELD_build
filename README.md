1481b377860d726e49a07d068bf69524b85c3516
Rusty.Benson <rusty.benson@noaa.gov>
2020-05-04 16:27:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# SHiELD_build
+
+This run/build environment is for use with the SHiELD code base and is
+an update to the original fvGFS build system.
+
+
+# Description
+
+Everything needed to build th SHiELD model on a given platform has been included.
+Supported platforms:
+  * Gaea
+  * Orion
+  * Hera
+  * Jet (untested)
+
+The build system builds the FMS library once and will use it for all subsequent
+builds.
+
+
+# Caveats
+
+There are special handling instructions in BUILD/mk_scritps/mk_makefile for
+the following files:
+  * GFS_diagnostics
+     - compiles at -O0 to improve compilation speed
+  * radiation_aerosols.f
+     - reduced to CORE-AVX-I to ensure reproducibility
+     - may no longer be needed (is it fixed in newer versions of Intel??)
+  * nh_utils.F90
+     - adds --fast-transcendentals for performance
+  * fv_mapz.F90
+     - adds --fast-transcendentals for performance
+
+
+# Instructions
+
+Process for building SHiELD
+
+1. Modify/run CHECKOUT_code
+   * tags/branches from GitHub and GitLab
+2. Ensure the platform you are using is supported in site/environment.sh
+   * update modules
+   * update compiler selections
+   * update AVX support
+3. Ensure you have the mkmf submodule
+   * git submodule update --init
+   * git submodule update --remote
+4. In the Build directory, execute either
+   * COMP_dev [clean]
+     - builds a specific configuation
+     - allows for recursive use when omitting argument
+   * COMP_all
+     - creates executables for a set of configurations
+     - always performs a clean compile
+
+
+# Disclaimer
+
+This project is provided on an "as is" basis and the user assumes responsibility
+for its use. Any claims against the Department of Commerce stemming from the
+use of this project will be governed by all applicable Federal law. Any reference to
+specific commercial products, processes, or services by service mark, trademark,
+manufacturer, or otherwise, does not constitute or imply their endorsement,
+recommendation or favoring by the Department of Commerce.
+
+This project code is made available through GitLab and is managed
+at https://gitlab.gfdl.noaa.gov
---
+++ site/environment.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+if [ `hostname | cut -c1-5` = "Orion" ] ; then
+   echo " Orion environment "
+
+   . ${MODULESHOME}/init/sh
+   module load intel/2020
+   module load impi/2020
+   module load netcdf
+   module load hdf5
+
+   export CPATH="${NETCDF}/include:${CPATH}"
+   export HDF5=${HDF5_ROOT}
+   export LIBRARY_PATH="${LIBRARY_PATH}:${NETCDF}/lib:${HDF5}/lib"
+   export NETCDF_DIR=${NETCDF}
+
+   # make your compiler selections here
+   export FC=mpiifort
+   export CC=mpiicc
+   export CXX=mpicpc
+   export LD=mpiifort
+   export TEMPLATE=site/intel.mk
+
+   # highest level of AVX support
+   export AVX_LEVEL=-xSKYLAKE-AVX512
+
+
+elif [ `hostname | cut -c1-4` = "gaea" ] ; then
+   echo " gaea environment "
+
+   . ${MODULESHOME}/init/sh
+   module unload PrgEnv-pgi
+   module load   PrgEnv-intel
+   module rm intel
+   module load intel/19.0.5.281
+   module load cray-netcdf
+   module load craype-hugepages4M
+
+   # make your compiler selections here
+   export FC=ftn
+   export CC=cc
+   export CXX=CC
+   export LD=ftn
+   export TEMPLATE=site/intel.mk
+
+   # highest level of AVX support
+   export AVX_LEVEL=-xCORE-AVX2
+
+
+elif [ `hostname | cut -c1-2` = "fe" || `hostname | cut -c1` = "x" ] ; then
+   echo " jet environment "
+
+   . ${MODULESHOME}/init/sh
+   module purge
+   module load newdefaults
+   module load intel/2016.2.181 # Jet's default is 15.0.3.187, but this one is 16.0.2.181
+   module load szip/2.1
+   module load hdf5/1.8.9
+   module load netcdf4/4.2.1.1
+   module load mvapich2/2.1
+
+   export LIBRARY_PATH="${LIBRARY_PATH}:${NETCDF4}/lib:${HDF5}/lib"
+   export NETCDF_DIR=${NETCDF4}
+
+   # make your compiler selections here
+   export FC=mpiifort
+   export CC=mpiicc
+   export CXX=mpicpc
+   export LD=mpiifort
+   export TEMPLATE=site/intel.mk
+
+
+elif [ `hostname | cut -c1` = "h" ] ; then
+   echo " hera environment "
+
+   source $MODULESHOME/init/sh
+   module load intel/15.1.133
+   module load netcdf/4.3.0
+   module load hdf5/1.8.14
+
+   export LIBRARY_PATH="${LIBRARY_PATH}:${NETCDF}/lib:${HDF5}/lib"
+   export NETCDF_DIR=${NETCDF}
+
+   # make your compiler selections here
+   export FC=mpiifort
+   export CC=mpiicc
+   export CXX=mpicpc
+   export LD=mpiifort
+   export TEMPLATE=site/intel.mk
+
+   # highest level of AVX support
+   export AVX_LEVEL=-xSKYLAKE-AVX512
+
+
+else
+
+   echo " no environment available based on the hostname "
+
+fi
+
